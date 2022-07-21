ce16cae41f04fb536dd2adcfdfdffd764a6ce95e
Moshi <li.moshi95@gmail.com>
2022-07-20 21:48:26 -0400

    dummy server

--- main.tf
+++ main.tf
@@ -12,21 +12,9 @@ provider "aws" {
 }
 
 resource "aws_vpc" "fair_fairytale_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "fairFairytaleVpc"
-  }
-}
-
-resource "aws_subnet" "fair_fairytale_subnet" {
-  vpc_id     = aws_vpc.fair_fairytale_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "fairFairytaleSubnet"
   }
 }
 
@@ -38,12 +26,62 @@ resource "aws_internet_gateway" "fair_fairytale_gateway" {
   }
 }
 
+resource "aws_route_table" "fair_fairytale_route_table" {
+  vpc_id = aws_vpc.fair_fairytale_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fair_fairytale_gateway.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.fair_fairytale_gateway.id
+  }
+
+  tags = {
+    Name = "fairFairytaleRouteTable"
+  }
+}
+
+
+resource "aws_subnet" "fair_fairytale_subnet" {
+  vpc_id     = aws_vpc.fair_fairytale_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "fairFairytaleSubnet"
+  }
+}
+
+resource "aws_route_table_association" "fair_fairytale_route_table_association" {
+  subnet_id      = aws_subnet.fair_fairytale_subnet.id
+  route_table_id = aws_route_table.fair_fairytale_route_table.id
+}
+
+
 resource "aws_security_group" "fair_fairytale_security_group" {
   name        = "fair_fairytale_security_group"
-  description = "Allow SSH inbound connections"
+  description = "Allow SSH HTTP HTTPS connections"
   vpc_id = aws_vpc.fair_fairytale_vpc.id
 
   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -51,10 +89,10 @@ resource "aws_security_group" "fair_fairytale_security_group" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -62,17 +100,43 @@ resource "aws_security_group" "fair_fairytale_security_group" {
   }
 }
 
+resource "aws_network_interface" "fair_fairytale_network_interface" {
+  subnet_id      = aws_subnet.fair_fairytale_subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.fair_fairytale_security_group.id]
+
+}
+
+resource "aws_eip" "fair_fairytale_elastic_ip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.fair_fairytale_network_interface.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.fair_fairytale_gateway]
+}
+
+output "server_public_ip" {
+  value = aws_eip.fair_fairytale_elastic_ip.public_ip
+}
+
+
 resource "aws_instance" "fair_fairytale_server" {
 
-  instance_type = "t2.micro"
   ami = "ami-0b0ea68c435eb488d"
   #https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#LaunchInstances:ami=ami-0b0ea68c435eb488d
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "my-key"
 
-
-vpc_security_group_ids = [ aws_security_group.fair_fairytale_security_group.id ]
-  subnet_id = aws_subnet.fair_fairytale_subnet.id
-  associate_public_ip_address = true
-  tags = {
-    Name = "fairFairytaleServer"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.fair_fairytale_network_interface.id
   }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo Hello, Ready for Fair Fairytale? > /var/www/html/index.html'
+                EOF
 }
